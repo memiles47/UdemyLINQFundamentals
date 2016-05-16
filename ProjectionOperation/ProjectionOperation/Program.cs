0dc9b17ae0315c0c349156e5ae6a8f2cc59a5c3d
Michael Miles <boat081062@gmail.com>
2016-05-16 14:22:05 -0500

    These LINQ course will need to be backed up with some serious reading material

---
+++ ProjectionOperation/ProjectionOperation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectionOperation
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var cities = new List<string>()
+            {
+                "New York",
+                "Istanbul",
+                "Paris",
+                "London",
+                "Madrid",
+                "Monaco",
+                "Owasso",
+                "Seattle"
+            };
+
+            //var selectedCities = from city in cities
+            //    select city;
+
+            var selectedCities = cities.Select((city, index) => new { city, index });
+
+            Console.WriteLine("Cities into collection");
+            Console.WriteLine("----------------------");
+
+            //foreach (var i in selectedCities)
+            //    Console.WriteLine($"City Name: {i}");
+            //Console.ReadKey();
+
+            foreach (var i in selectedCities)
+                Console.WriteLine($"City Name: {i.city}\tIndex: {i.index}");
+            Console.ReadKey();
+        }
+    }
+}
