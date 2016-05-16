9e335f3d7b1acfd96d763e5abe1caff5352ade95
Michael Miles <boat081062@gmail.com>
2016-05-16 06:11:53 -0500

    This course is starting out very basic.

---
+++ DelegateLambdaSample/DelegateLambdaSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DelegateLambdaSample
+{
+    class Program
+    {
+        delegate int MyDelegate(int x, int y);
+
+        static void Main()
+        {
+            MyDelegate _delegate = MyMethod;
+            var a = _delegate(5, 10);
+
+            Console.WriteLine($"The value of \'a\' is: {a}");
+            Console.ReadKey();
+
+        }
+
+        static int MyMethod(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
