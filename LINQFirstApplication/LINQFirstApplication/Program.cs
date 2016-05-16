ca518ce3a8f5315e6b35c11f60940c5bad99df28
Michael Miles <boat081062@gmail.com>
2016-05-16 05:01:55 -0500

    First LINQ expressions

---
+++ LINQFirstApplication/LINQFirstApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQFirstApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = new List<int>() {15, 64, 747, 65, 14};
+
+            var result = from number in numbers
+                         where number < 50
+                select number;
+
+            foreach (var item in result)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
