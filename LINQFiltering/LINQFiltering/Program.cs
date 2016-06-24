4fe8ad67ed7001cc3d4889f42b70baacbc5b7c75
Michael E Miles <michael@miles-me.net>
2016-06-24 16:50:15 -0500

    Mor linq

---
+++ LINQFiltering/LINQFiltering/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQFiltering
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = new List<int> {15, 24, 40, 32, 55};
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+            foreach (var num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
