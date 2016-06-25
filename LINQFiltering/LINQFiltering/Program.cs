feaccd4f68ac4af82a085645712b04afdd45963a
Michael E Miles <michael@miles-me.net>
2016-06-24 19:34:07 -0500

    he

--- LINQFiltering/LINQFiltering/Program.cs
+++ LINQFiltering/LINQFiltering/Program.cs
@@ -9,13 +9,13 @@ namespace LINQFiltering
         static void Main()
         {
             var numbers = new List<int> {15, 24, 40, 32, 55};
-            var evenNumbers = numbers.Where(n => n % 2 == 0);
-            foreach (var num in evenNumbers)
-            {
-                Console.WriteLine(num);
-            }
-            Console.ReadLine();
 
+            var evenNumbers = numbers.Where((n,i) => (n % 2 == 0) && (i > 24));
+
+            foreach (var num in evenNumbers)
+                Console.WriteLine(num);
+
+            Console.ReadLine();
         }
     }
 }
